ce1dfb7551f5805e74dc3663b6c77c56a4f2c434
adharbert <adharbert@gmail.com>
2018-11-28 11:58:17 -0500

    Adding packages, store and reducers.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../store';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>My React App</h1>
-            </div>
+            <Provider store={store}>
+                <Router>
+                    <div className="App">
+                        <h1>My React App</h1>
+                    </div>
+                </Router>
+            </Provider>
         )
     }
 }
